5da9fa0615d5a92bfbcd827a857920f461739cff
Milad Taghavi <mitghi@me.com>
2018-06-20 11:28:12 +0200

    Update License

--- lfring.go
+++ lfring.go
@@ -1,7 +1,7 @@
 /*
 * MIT License
 *
-* Copyright (c) 2017 Mike Taghavi <mitghi[at]me.com>
+* Copyright (c) 2017 Milad (Mike) Taghavi <mitghi[at]me/gmail.com>
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
